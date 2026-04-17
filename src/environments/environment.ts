81fc38f06c1872f111cb43d35c3974e7cc9a16b2
Josefina Rivero <josefinarivero03@gmail.com>
2026-04-17 18:29:11 -0300

    URL nueva

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // Con ROOT_PATH=/api en el backend, apuntamos al prefijo /api en local
-  apiUrl: 'https://goofball-fiddling-obstacle.ngrok-free.dev'
+  apiUrl: 'https://ivy-earlobe-division.ngrok-free.dev',
 };
